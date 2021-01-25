c19cc91208d52ec6c17f61603ded8d6f9b2fd52b
Jens Peter Schroer <jens@blixt.tech>
2021-01-25 10:58:33 +0100

    Including Breaker Manager in UI

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,28 +1,46 @@
 #include "mainwindow.hpp"
 #include "mainwindowui.hpp"
+#include "breakermanager.hpp"
+#include "breaker.hpp"
+#include <QDebug>
 
 class MainWindow::PrivateData : QObject
 {
 public:
     PrivateData(Config *config, QObject *parent = nullptr) : QObject(parent)
     {
-        this->config = config;
-        this->ui = new MainWindowUI();
+        config = config;
+        ui = new MainWindowUI();
+        breakerManager = new BreakerManager(parent);
     }
 
     ~PrivateData()
     {
+        if (ui) {
+            delete ui;
+            ui = nullptr;
+        }
+
+        if (breakerManager) {
+            delete breakerManager;
+            breakerManager = nullptr;
+        }
 
     }
 
     Config *config;
     MainWindowUI *ui;
+    BreakerManager *breakerManager;
 };
 
 MainWindow::MainWindow(Config *config)
 {
     pData_ = new PrivateData(config, this);
     pData_->ui->setupUi(this);
+    QObject::connect(pData_->breakerManager, &BreakerManager::breakerFound, 
+                    this, &MainWindow::onBreakerFound);
+    QObject::connect(pData_->breakerManager, &BreakerManager::breakerNotFound, 
+                    this, &MainWindow::onBreakerNotFound);
 }
 
 MainWindow::~MainWindow()
@@ -31,4 +49,31 @@ MainWindow::~MainWindow()
         delete pData_;
         pData_ = NULL;
     }
+}
+
+void MainWindow::showEvent(QShowEvent* event)
+{
+    QHostAddress host;
+
+    // Add address for searching
+    host.setAddress("10.42.0.255");
+    pData_->breakerManager->addToSearch(host);
+
+    // Add address of a known breaker
+    host.setAddress("10.42.0.45");
+    pData_->breakerManager->addBreaker(host);
+
+    pData_->breakerManager->searchStart();
+}
+
+void MainWindow::onBreakerFound(Breaker *breaker)
+{
+    qInfo() << "Breaker found. UUID:" << breaker->uuid() 
+            << "IP:" << breaker->host().toString()
+            << "MAC:" << breaker->hardwareAddress();
+}
+
+void MainWindow::onBreakerNotFound(QHostAddress &host, int port)
+{
+    qCritical() << "Breaker not found at address" << host.toString() << "and port" << port;
 }
--- src/mainwindow.hpp
+++ src/mainwindow.hpp
@@ -3,7 +3,10 @@
 
 #include "common.hpp"
 #include <QMainWindow>
+#include <QVector>
+#include <QHostAddress>
 
+class Breaker;
 class MainWindow : public QMainWindow {
 
     Q_OBJECT
@@ -11,7 +14,12 @@ public:
     MainWindow(Config *config);
     ~MainWindow();
 
+private slots:
+    void onBreakerFound(Breaker *breaker);
+    void onBreakerNotFound(QHostAddress &host, int port);
+
 private:
+    void showEvent(QShowEvent* event);
     class PrivateData;
     PrivateData* pData_; 
 };
