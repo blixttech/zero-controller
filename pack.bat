3936959353e42f3b2c86cabfeb4264ae99ef1771
Jens Peter Schroer <jens@blixt.tech>
2024-03-15 16:03:39 +0100

    adding qwt to pack by hand

--- pack.bat
+++ pack.bat
@@ -2,6 +2,7 @@
 cd %~dp0\artifact\Release\bin
 
 windeployqt zero-controller.exe --release --compiler-runtime
+copy %Qt6_DIR%\lib\qwt.dll .
 copy C:\Windows\System32\concrt140.dll . 
 copy C:\Windows\System32\vccorlib140.dll .
 copy C:\Windows\System32\msvcp140.dll .
