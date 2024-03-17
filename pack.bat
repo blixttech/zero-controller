4e04de705ad3e8e26c4af2760dbb2c1405273c4e
Jens Peter Schroer <jens@blixt.tech>
2024-03-17 17:50:13 +0100

    adding missing protobuf dll

--- pack.bat
+++ pack.bat
@@ -7,5 +7,7 @@ copy C:\Windows\System32\concrt140.dll .
 copy C:\Windows\System32\vccorlib140.dll .
 copy C:\Windows\System32\msvcp140.dll .
 copy C:\Windows\System32\vcruntime140.dll .
+copy C:\vcpkg\packages\protobuf_x64-windows\lib\protobuf.dll .
+
 7z a C:\zero-controller-win64.zip *          
 
