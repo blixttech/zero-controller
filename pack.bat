06d8649bfd002bad22b148154b71f1a16f2c54bd
Jens Peter Schroer <jens@blixt.tech>
2025-04-19 16:18:15 +0200

    Fixing Windows build

--- pack.bat
+++ pack.bat
@@ -9,7 +9,4 @@ copy C:\Windows\System32\vccorlib140.dll .
 copy C:\Windows\System32\msvcp140.dll .
 copy C:\Windows\System32\vcruntime140.dll .
 
-copy C:\vcpkg\buildtrees\protobuf\x64-windows-rel\libprotobuf.dll .
-
-7z a C:\zero-controller-win64.zip *          
-
+7z a C:\zero-controller-win64.zip * 
